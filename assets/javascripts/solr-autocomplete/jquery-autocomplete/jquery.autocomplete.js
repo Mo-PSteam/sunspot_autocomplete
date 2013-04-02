02cc225843d3a28342a40782b8ff216cf954e9e8
Mo-PSteam <mo@theoffice.ps>
2013-04-02 18:46:34 +0200

    Update jquery.autocomplete.js

--- assets/javascripts/solr-autocomplete/jquery-autocomplete/jquery.autocomplete.js
+++ assets/javascripts/solr-autocomplete/jquery-autocomplete/jquery.autocomplete.js
@@ -1,5 +1,5 @@
 /*
- *  This part of the library is extended from jQuery Autocomplete plugin by Jörn Zaefferer 
+ *  This part of the library is extended from jQuery Autocomplete plugin by Jï¿½rn Zaefferer 
  */
 
 
@@ -107,17 +107,8 @@ $.Autocompleter = function(input, fieldName, options) {
 	  solrUrl: options.url
 	});
 	ajaxSolrManager.init();
-	
-	// prevent form submit in opera when selecting with return key
-	$.browser.opera && $(input.form).bind("submit.autocomplete", function() {
-		if (blockSubmit) {
-			blockSubmit = false;
-			return false;
-		}
-	});
-	
-	// only opera doesn't trigger keydown multiple times while pressed, others don't work with keypress at all
-	$input.bind(($.browser.opera ? "keypress" : "keydown") + ".autocomplete", function(event) {
+
+	$input.bind("keydown.autocomplete", function(event) {
 		// a keypress means the input has focus
 		// avoids issue where input had focus before the autocomplete was applied
 		hasFocus = 1;
